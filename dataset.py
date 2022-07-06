bfd173f9bca00309d56515a7bbef24c0b55c24c3
EduardE02 <eduard@rantsevich.net>
2022-07-06 05:22:19 +0200

    some basic data augmentation (offset image)

--- dataset.py
+++ dataset.py
@@ -1,6 +1,9 @@
 import mnist
 import utils
 from cifar10_web import cifar10
+from matplotlib import pyplot as plt
+import cv2
+import numpy as np
 
 
 def get_train_data_mnist():
@@ -31,3 +34,32 @@ def get_test_data_cifar10():
     train_images, train_labels, test_images, test_labels = cifar10(path="C:/Users/Eduard/data/cifar10")
     test_images = test_images.astype("float64")
     return test_images, test_labels
+
+
+def data_augment_mnist(data, offset):
+    # assume square image
+    if len(data.shape) < 3:
+        side_len = int(data.shape[-1]**0.5)
+        data = data.reshape((-1, side_len, side_len))
+    original_data = data
+    data = np.concatenate((data, shift_data(original_data, offset, 0)), axis=0)
+    data = np.concatenate((data, shift_data(original_data, -offset, 0)), axis=0)
+    data = np.concatenate((data, shift_data(original_data, 0, offset)), axis=0)
+    data = np.concatenate((data, shift_data(original_data, 0, -offset)), axis=0)
+    print(data.shape)
+    return data
+
+
+def shift_data(data, shift_x, shift_y):
+    new_data = np.copy(data)
+    if shift_x > 0:
+        new_data[:,:, shift_x:] = data[:,:, :-shift_x]
+    elif shift_x < 0:
+        new_data[:,:, :shift_x] = data[:, :, shift_x * -1:]
+    elif shift_y > 0:
+        new_data[:,shift_y:, :] = data[:,:-shift_y, :]
+    else:
+        new_data[:,:shift_y, :] = data[:, shift_y * -1:, :]
+    return new_data
+
+
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import model as model_file
 import dataset
 import layers
@@ -6,15 +8,17 @@ import layers
 if __name__ == "__main__":
     input_size, output_size = 784, 10
     model = model_file.Model("Cool model")
-    model.add(layers.FullyConnected(800, activation="relu", use_bias=True, input_shape=(1, input_size)))
+    model.add(layers.FullyConnected(300, activation="relu", use_bias=True, input_shape=(1, input_size)))
+    model.add(layers.FullyConnected(300, activation="relu", use_bias=True))
     model.add(layers.FullyConnected(50, activation="relu", use_bias=True))
     model.add(layers.FullyConnected(10, activation="softmax", use_bias=False))
     model.finish(1e-3, "adam", "cross_entropy")
     # model.load_model_weights()
     train_samples, train_labels = dataset.get_train_data_mnist()
+    train_samples, train_labels = dataset.data_augment_mnist(train_samples, 5).reshape(-1, 784), np.concatenate([train_labels for i in range(5)], axis=0)
     test_samples, test_labels = dataset.get_test_data_mnist()
 
-    model.train(train_samples, train_labels, batch_size=512, epochs=30,
+    model.train(train_samples, train_labels, batch_size=512, epochs=10,
                 shuffle=True
                 # , validation_samples=test_samples, validation_labels=test_labels
                 )
